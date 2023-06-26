9d050580b681024ec4208c274aaf33408c9a0b3f
marcosxk122 <marcost.santana@hotmail.com>
2023-06-26 10:39:40 -0300

    StatusBar

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 
@@ -6,8 +6,9 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View>
-      {fontsLoaded ? <Text>Hello World!</Text> : <View/>}
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024'}}>
+      <StatusBar barStyle="light-content" backgroundColor='transparent' translucent/>
+      {fontsLoaded ? <Text style={{fontFamily: 'Roboto_700Bold'}}>Hello World!</Text> : <View/>}
     </View>
   );
 }
