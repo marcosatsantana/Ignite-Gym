991b1b106985cdf158614336ca6cfbada1ad544a
marcosxk122 <marcost.santana@hotmail.com>
2023-06-27 12:28:00 -0300

    creating header home component

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,10 +1,12 @@
 import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 import { Platform } from 'react-native'
 import { useTheme } from 'native-base';
+
 import Home from '@screens/Home';
 import Profile from '@screens/Profile';
 import History from '@screens/History';
 import Exercise from '@screens/Exercise';
+
 import HomeSvg from '@assets/home.svg';
 import HistorySvg from '@assets/history.svg';
 import ProfileSvg from '@assets/profile.svg';
