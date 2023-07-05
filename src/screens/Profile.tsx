bd6206f518de932d0be60766da1e0f99e22388bf
marcosxk122 <marcost.santana@hotmail.com>
2023-07-05 11:57:58 -0300

    Profile Form Validation

---
+++ src/screens/Profile.tsx
@@ -0,0 +1,204 @@
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from 'native-base';
+import { Controller, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+import * as yup from 'yup';
+
+import { useAuth } from '@hooks/useAuth';
+
+import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+
+
+const PHOTO_SIZE = 33;
+
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  old_password: string;
+  confirm_password: string;
+}
+
+const profileSchema = yup.object({
+  name: yup.string().required('Informe o nome'),
+  password: yup.string().min(6, 'Minimo de 6 digitos necessarios').nullable().transform((value)=> !!value ? value : null),
+  confirm_password: yup.string().nullable().transform((value)=> !!value ? value : null).oneOf([yup.ref('password'), null], 'A confirmação de senha não bate')
+})
+
+export function Profile() {
+
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://github.com/marcosatsantana.png');
+
+  const toast = useToast();
+  const { user } = useAuth();
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({ 
+    defaultValues: { 
+      name: user.name,
+      email: user.email
+    },
+    resolver: yupResolver(profileSchema) 
+  });
+
+  async function handleUserPhotoSelected(){
+    setPhotoIsLoading(true);
+    
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+  
+      if(photoSelected.cancelled) {
+        return;
+      }
+
+      if(photoSelected.uri) {
+
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.uri);
+        
+        if(photoInfo.size && (photoInfo.size  / 1024 / 1024 ) > 2){
+          
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500'
+          })
+        }
+
+        setUserPhoto(photoSelected.uri);
+      }
+  
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setPhotoIsLoading(false)
+    }
+  }
+
+  async function handleProfileUpdate(data: FormDataProps) {
+    console.log(data);
+  }
+ 
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title='Perfil' />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt={6} px={10}>
+          {
+            photoIsLoading ?
+              <Skeleton 
+                w={PHOTO_SIZE}
+                h={PHOTO_SIZE}
+                rounded="full"
+                startColor="gray.500"
+                endColor="gray.400"
+              />
+            :
+              <UserPhoto 
+                source={{ uri: userPhoto }}
+                alt="Foto do usuário"
+                size={PHOTO_SIZE}
+              />
+          }
+          
+          <TouchableOpacity onPress={handleUserPhotoSelected}>
+            <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+              Alterar Foto
+            </Text>
+          </TouchableOpacity>
+
+          <Controller 
+            control={control}
+            name="name"
+            render={({ field: { value, onChange } }) => (
+              <Input 
+                bg="gray.600" 
+                placeholder='Nome'
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.name?.message}
+              />
+            )}
+          />
+
+          <Controller 
+            control={control}
+            name="email"
+            render={({ field: { value, onChange } }) => (
+              <Input 
+                bg="gray.600" 
+                placeholder="E-mail"
+                isDisabled
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          
+        
+          <Heading color="gray.200" fontSize="md" mb={2} alignSelf="flex-start" mt={12} fontFamily="heading">
+            Alterar senha
+          </Heading>
+
+          <Controller 
+            control={control}
+            name="old_password"
+            render={({ field: { onChange } }) => (
+              <Input 
+                bg="gray.600"
+                placeholder="Senha antiga"
+                secureTextEntry
+                onChangeText={onChange}
+              />
+            )}
+          />
+
+          <Controller 
+            control={control}
+            name="password"
+            render={({ field: { onChange } }) => (
+              <Input 
+                bg="gray.600"
+                placeholder="Nova senha"
+                secureTextEntry
+                onChangeText={onChange}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
+
+          <Controller 
+            control={control}
+            name="confirm_password"
+            render={({ field: { onChange } }) => (
+              <Input 
+                bg="gray.600"
+                placeholder="Confirme a nova senha"
+                secureTextEntry
+                onChangeText={onChange}
+                errorMessage={errors.confirm_password?.message}
+              />
+            )}
+          />
+
+          <Button 
+            title="Atualizar" 
+            mt={4} 
+            onPress={handleSubmit(handleProfileUpdate)}
+          />
+        </Center>
+      </ScrollView>
+    </VStack>
+  );
+}
